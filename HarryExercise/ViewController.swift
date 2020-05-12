a56fde45a334382d8c2abb2ecede255d66861607
johan.fornander <johan.fornander@playtech.com>
2020-05-12 15:48:58 +0200

    moving project to github

---
+++ HarryExercise/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  HarryExercise
+//
+//  Created by Johan Fornander on 2020-05-12.
+//  Copyright © 2020 Johan Fornander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        print("hello world")
+    }
+
+
+}
+
